773abacbc074599a1b24cb5f57d69bd121f02ae1
LucasOliveiraDantas <lucasdeoliveiradantas@protonmail.com>
2022-09-29 14:08:42 -0300

    ajustes

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const iconShare = document.querySelector(".icon-share")
+const iconShareM = document.querySelector(".icon-share-m")
 const shareArea = document.querySelector(".share")
 
 iconShare.addEventListener('click', function(){
