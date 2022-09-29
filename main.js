ab0c3e0b617e6fd4cb2e8d963f7738a198ca725a
LucasOliveiraDantas <lucasdeoliveiradantas@protonmail.com>
2022-09-29 14:19:50 -0300

    ajustes

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const InfoPerfil = document.querySelector(".section-perfil")
+
 const iconShare = document.querySelector(".icon-share")
 const iconShareM = document.querySelector(".icon-share-m")
 const shareArea = document.querySelector(".share")
@@ -5,4 +7,11 @@ const shareArea = document.querySelector(".share")
 iconShare.addEventListener('click', function(){
     shareArea.classList.toggle('active')
     iconShare.classList.toggle('active')
+    InfoPerfil.classList.toggle('off')
 }) 
+
+iconShareM.addEventListener('click', function(){
+    shareArea.classList.remove('active')
+    iconShare.classList.remove('active')
+    InfoPerfil.classList.remove('off')
+})
